cfa1f9a6d5b9d058d31cd062deda76d95f733a50
HARJEET <harjeet20561@iiitd.ac.in>
2022-01-11 02:42:53 +0530

    CustomException class

---
+++ Will_Hero/src/InsufficientCoinException.java
@@ -0,0 +1,15 @@
+public class InsufficientCoinException extends Exception{
+
+    public InsufficientCoinException(String message){
+
+        super(message);
+    }
+}
+
+class InvalidRessurection extends Exception{
+
+    public InvalidRessurection(String message){
+
+        super(message);
+    }
+}
